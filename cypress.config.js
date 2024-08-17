65999784b6dc5935a5048fbca65ae691b2c8d474
adevair.junior <profadevairvitorio@gmail.com>
2024-08-17 18:01:59 -0300

    feat: config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:8080',
+    supportFile: false,
+    video: false,
+    screenshotsFolder: 'cypress/screenshots',
+    defaultCommandTimeout: 8000,
   },
 });
